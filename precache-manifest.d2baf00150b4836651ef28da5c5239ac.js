b513a908922978646ed6acce613409e386ff47be
lin <1248075286@qq.com>
2018-06-14 19:42:22 +0800

    make my home

---
+++ precache-manifest.d2baf00150b4836651ef28da5c5239ac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "8a56c75f7ed51a1511f625fa0c751b67",
+    "url": "/static/bmob-min.js"
+  },
+  {
+    "revision": "20455bccd878b779a3f3",
+    "url": "/js/vendors~app.20455bcc.js"
+  },
+  {
+    "revision": "bb27c1f1453aea9a390e",
+    "url": "/js/app.bb27c1f1.js"
+  },
+  {
+    "revision": "df96b139ff345ce33adbc7a0154800ab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d18f179f1224d04853de4dc780c68382",
+    "url": "/img/iconfont.d18f179f.svg"
+  },
+  {
+    "revision": "20455bccd878b779a3f3",
+    "url": "/css/vendors~app.0.f59ea31e.css"
+  },
+  {
+    "revision": "bb27c1f1453aea9a390e",
+    "url": "/css/app.cc7b041a.css"
+  }
+];
